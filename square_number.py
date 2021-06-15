41ab9cad83eade74d2050971b5b357fe69c3b395
Alejandro Delgado <69034459+alejandrodf1@users.noreply.github.com>
2021-06-15 15:36:50 -0500

    Programa optimizado

--- square_number.py
+++ square_number.py
@@ -2,9 +2,8 @@ def run():
     result = []
 
     for number in range (1, 101):
-        operation = number ** 2
-        if operation%3 != 0:
-            result.append(operation)
+        if number%3 != 0:
+            result.append(number ** 2)
     
     print(result)
 
