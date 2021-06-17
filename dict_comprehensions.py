6da21ddafdd0fa9aa6bca15469ef41b774660b21
Alejandro Delgado <69034459+alejandrodf1@users.noreply.github.com>
2021-06-16 22:41:05 -0500

    Terminado con modelo de dicccionary comprehension

--- dict_comprehensions.py
+++ dict_comprehensions.py
@@ -21,8 +21,8 @@ def run():
     #          my_dict[i] = i**3
 
 
-    dicci = {i:i**3 for i in range(1,101) if i%3 != 0}  ##forma eficiente de hacer lo de arriba
-    print(dicci)         
+    # dicci = {i:i**3 for i in range(1,101) if i%3 != 0}  ##forma eficiente de hacer lo de arriba
+    # print(dicci)         
 
     my_dict = {i:i**0.5 for i in range(1, 1001)}
     print(my_dict)
