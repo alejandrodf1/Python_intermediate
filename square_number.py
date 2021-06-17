24589f2a497622b0f0456fa693bd8fafea0304bc
Alejandro Delgado <69034459+alejandrodf1@users.noreply.github.com>
2021-06-16 20:35:12 -0500

    terminamos proyecto de cuadrados

--- square_number.py
+++ square_number.py
@@ -1,11 +1,17 @@
 def run():
-    result = []
+    # result = []
 
-    for number in range (1, 101):
-        if number%3 != 0:
-            result.append(number ** 2)
+    # for number in range (1, 101):
+    #     if number%3 != 0:
+    #         result.append(number ** 2)
     
-    print(result)
+    # squares = [i**2 for i in range(1, 101) if i % 3 != 0]
+
+    # multiples = [i for i in range(1, 99999) if i%4==0 and i%6==0 and i%9 == 0]
+
+    multiples = [i for i in range(1, 99999) if i%36 == 0]
+
+    print(multiples)
 
 if __name__ == '__main__':
     run()
