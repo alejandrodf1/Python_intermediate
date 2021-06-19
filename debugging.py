ad6c32b5b86aa9f1c3d539a8a43ab44ddae91ff4
Alejandro Delgado <69034459+alejandrodf1@users.noreply.github.com>
2021-06-18 19:39:53 -0500

    Exploramos el metodo clasico de enfrentar errores

--- debugging.py
+++ debugging.py
@@ -1,5 +1,20 @@
-def run():
-    pass
+def divisiors(number):
+    try:
+        if number < 0:
+            raise ValueError ('Debes ingresar numeros enteros positivos')
+        divisior = [i for i in range(1, number+1) if number%i == 0]
+        return divisior
+    except ValueError as ve:
+        print(ve)
+        return False
 
-if __name__=='__main__':
+
+def run():
+    try:
+        number = int(input('Ingresa un numero: '))
+        print(divisiors(number))
+        print('termino programa')
+    except ValueError: 
+        print('Debes ingresar un numero')
+if __name__ == '__main__':
     run()
